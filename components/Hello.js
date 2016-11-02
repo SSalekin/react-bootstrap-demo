cc6bd965a3135bf595e33e5b26265545a963d5ce
SSalekin <sjsalekin@gmail.com>
2016-11-02 13:28:17 +0600

    collapse

--- components/Hello.js
+++ components/Hello.js
@@ -1,17 +1,58 @@
-import React, { PropTypes } from 'react'
+import React, { PropTypes } from 'react';
+import { Button, Collapse, Form, FormGroup, FormControl, Col, ControlLabel, Checkbox } from 'react-bootstrap';
 
-const Hello = ({ onClick, message }) => {
-  return (
-    <div>
-      <h1>{ message }</h1>
-      <button onClick={onClick}>Click</button>
-    </div>
-  )
-}
+class Hello extends React.Component {
+  constructor(...args) {
+    super(...args);
+    this.state = {};
+  }
 
-Hello.propTypes = {
-  onClick: PropTypes.func.isRequired,
-  message: PropTypes.string.isRequired
+  render() {
+    return (
+      <div className="container">
+        <Button onClick={ ()=> this.setState({ open: !this.state.open })}>
+          Toggle
+        </Button>
+        <Collapse in={this.state.open}>
+          <div className="well">
+            <Form horizontal>
+              <FormGroup controlId="formHorizontalEmail">
+                <Col componentClass={ControlLabel} sm={2}>
+                  Email
+                </Col>
+                <Col sm={10}>
+                  <FormControl type="email" placeholder="Email" />
+                </Col>
+              </FormGroup>
+
+              <FormGroup controlId="formHorizontalPassword">
+                <Col componentClass={ControlLabel} sm={2}>
+                  Password
+                </Col>
+                <Col sm={10}>
+                  <FormControl type="password" placeholder="Password" />
+                </Col>
+              </FormGroup>
+
+              <FormGroup>
+                <Col smOffset={2} sm={10}>
+                  <Checkbox>Remember me</Checkbox>
+                </Col>
+              </FormGroup>
+
+              <FormGroup>
+                <Col smOffset={2} sm={10}>
+                  <Button type="submit">
+                    Sign in
+                  </Button>
+                </Col>
+              </FormGroup>
+            </Form>
+          </div>
+        </Collapse>
+      </div>
+    );
+  }
 }
 
 export default Hello
